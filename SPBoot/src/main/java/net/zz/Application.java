ba12bf3e1de5c19ad904834c9ff3a0ed7afd91bb
zhangzhuang <zhangzhuang@tycmc.net>
2019-11-08 10:33:10 +0800

    第一次

---
+++ SPBoot/src/main/java/net/zz/Application.java
@@ -0,0 +1,19 @@
+package net.zz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages="net.zz")
+public class Application extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Application.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
